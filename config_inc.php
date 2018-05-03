408a30d255696cb070670c2148b4785f6a871ee5
root <root@localhost.localdomain>
2018-05-03 17:59:05 -0400

    First commit

---
+++ config_inc.php
@@ -0,0 +1,11 @@
+<?php
+	$g_hostname = '127.0.0.1';
+	$g_db_type = 'mysql';
+	$g_database_name = 'bugtracker';
+	$g_db_username = 'root';
+	$g_db_password = '';
+        $g_file_upload_method   = DISK;
+        $g_absolute_path_default_upload_folder = '/var/www/issues/uploads';
+
+?>
+
